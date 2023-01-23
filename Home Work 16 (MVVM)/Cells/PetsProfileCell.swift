8f8e789783e624465ce01aa49fd9d63dbf784ef8
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-23 15:59:41 +0300

    firs View is completed

--- Home Work 16 (MVVM)/Cells/PetsProfileCell.swift
+++ Home Work 16 (MVVM)/Cells/PetsProfileCell.swift
@@ -36,6 +36,7 @@ class PetsProfileCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "All"
         label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .medium)
         return label
     }()
     
@@ -51,14 +52,18 @@ class PetsProfileCell: UICollectionViewCell {
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "1"
+        label.text = "Lorem ipsum dolor sit amet, "
+        label.textColor = .systemGray2
+        label.font = .systemFont(ofSize: 14)
         return label
     }()
     
     private lazy var priceLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "1"
+        label.text = "1$"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 20, weight: .bold)
         return label
     }()
     
@@ -67,10 +72,37 @@ class PetsProfileCell: UICollectionViewCell {
         let image = UIImage(named: "cart")
         imageView.image = image
         
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
+    private lazy var buttonAdd: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Add", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
     
+    private lazy var iconHeart: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(systemName: "heart.fill")
+        imageView.image = image
+        imageView.tintColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private lazy var iconReting: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "stars")
+        imageView.image = image
+        imageView.tintColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
     
     
     
@@ -95,6 +127,10 @@ class PetsProfileCell: UICollectionViewCell {
         viewContainer.addSubview(title)
         viewContainer.addSubview(priceLabel)
         viewContainer.addSubview(descriptionLabel)
+        viewContainer.addSubview(iconCart)
+        viewContainer.addSubview(buttonAdd)
+        viewContainer.addSubview(iconHeart)
+        viewContainer.addSubview(iconReting)
     }
     
     private func setupLayout() {
@@ -115,17 +151,32 @@ class PetsProfileCell: UICollectionViewCell {
             buttonLike.trailingAnchor.constraint(equalTo: viewContainer.trailingAnchor, constant: -5),
             buttonLike.topAnchor.constraint(equalTo: contentView.topAnchor),
             
-            title.topAnchor.constraint(equalTo: viewContainer.topAnchor, constant: 10),
+            title.topAnchor.constraint(equalTo: viewContainer.topAnchor, constant: 20),
             title.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
             
-            descriptionLabel.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 17),
+            descriptionLabel.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 8),
             descriptionLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
             
             priceLabel.bottomAnchor.constraint(equalTo: viewContainer.bottomAnchor, constant: -10),
             priceLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
             
+            iconCart.centerYAnchor.constraint(equalTo: priceLabel.centerYAnchor),
+            iconCart.trailingAnchor.constraint(equalTo: viewContainer.trailingAnchor, constant: -65),
+            iconCart.widthAnchor.constraint(equalToConstant: 20),
+            iconCart.heightAnchor.constraint(equalToConstant: 20),
             
+            buttonAdd.centerYAnchor.constraint(equalTo: iconCart.centerYAnchor),
+            buttonAdd.leadingAnchor.constraint(equalTo: iconCart.trailingAnchor, constant: 10),
             
+            iconHeart.centerXAnchor.constraint(equalTo: buttonLike.centerXAnchor),
+            iconHeart.centerYAnchor.constraint(equalTo: buttonLike.centerYAnchor),
+            iconHeart.heightAnchor.constraint(equalToConstant: 27),
+            iconHeart.widthAnchor.constraint(equalToConstant: 30),
+            
+            iconReting.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
+            iconReting.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 12),
+            iconReting.heightAnchor.constraint(equalToConstant: 20),
+            iconReting.widthAnchor.constraint(equalToConstant: 80)
         
         ])
     }
