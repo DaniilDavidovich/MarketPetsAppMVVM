a0c1b5fd8f82a6db49625e3dd10e2b43e8bfd2c5
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-23 13:28:59 +0300

    setup TextField

--- Home Work 16 (MVVM)/Cells/TextFieldCell.swift
+++ Home Work 16 (MVVM)/Cells/TextFieldCell.swift
@@ -11,16 +11,65 @@ class TextFieldCell: UICollectionViewCell {
     
     static var identifier = "TextFieldCell"
     
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.attributedPlaceholder = NSAttributedString(string: "Type something", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        let image = UIImage(systemName: "magnifyingglass")
+        textField.setLeftIcon(image!)
+        textField.backgroundColor = .white
+        textField.textAlignment = .left
+        textField.layer.cornerRadius = 25
+        return textField
+    }()
+    
     // MARK: - Initializers
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .gray
-        
+        setupView()
+        setupHierarhy()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Error in Cell")
     }
     
+    //MARK: - Setups
+    private func setupView() {
+        backgroundColor = .systemBlue
+    }
+    
+    private func setupHierarhy() {
+        addSubview(textField)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            textField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            textField.topAnchor.constraint(equalTo: contentView.topAnchor),
+            textField.heightAnchor.constraint(equalToConstant: 60),
+            textField.widthAnchor.constraint(equalToConstant: 363)
+            
+        ])
+    }
+    
 }
+
+
+extension UITextField {
+    
+    func setLeftIcon(_ image: UIImage) {
+        
+        let iconView = UIImageView(frame: CGRect(x: 25, y: 5, width: 30, height: 25))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 70, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
+
+
--- Home Work 16 (MVVM)/Source/ViewController.swift
+++ Home Work 16 (MVVM)/Source/ViewController.swift
@@ -26,15 +26,6 @@ class ViewController: UIViewController {
         return collection
     }()
     
-    private lazy var textField: UITextField = {
-        let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
-//        textField.placeholder = "Type something"
-        textField.font = .systemFont(ofSize: 12)
-        
-        return textField
-    }()
-    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -52,7 +43,6 @@ class ViewController: UIViewController {
     
     private func setupHierarhy() {
         view.addSubview(collectionView)
-        view.addSubview(textField)
     }
     
     private func setupLayout() {
@@ -61,11 +51,7 @@ class ViewController: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
-            
+                        
         ])
     }
 
