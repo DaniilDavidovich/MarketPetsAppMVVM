edc28ba3ff2822e1cfadc5d36a1b544c39d60b9b
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-23 16:13:50 +0300

    add detail view and push to detail view for tup in 3 section

---
+++ Home Work 16 (MVVM)/Source/ViewControllers/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Home Work 16 (MVVM)
+//
+//  Created by Daniil Davidovich on 23.01.23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
